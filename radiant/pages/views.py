0aa6a00e7efb98688a74ad4ddf0ae397dd039528
Victor Rocha <victor@rochapps.com>
2016-01-09 15:52:05 -0500

    [radiant-page] Cleans up view so that it doesn't 500

--- radiant/pages/views.py
+++ radiant/pages/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView
 
 from .models import RadiantPage
@@ -7,12 +8,7 @@ class RadiantPageView(DetailView):
     model = RadiantPage
 
     def get_object(self, queryset=None):
-        url = self.kwargs.get('url')
-        if url:
-            page = RadiantPage.objects.get(url=url)
-        else:
-            page = RadiantPage.objects.get(url='')
-        return page
+        return get_object_or_404(RadiantPage, url=self.kwargs.get('url'))
 
     def get_template_names(self):
         return ['pages/index.html']
