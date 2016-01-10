f8b34ffcd10c25b92a140ddc9b2fcbe7be48ed59
Victor Rocha <victor@rochapps.com>
2016-01-10 11:30:38 -0500

    [ogtags] Add description to ogtags fields in the admin

--- radiant/common/models.py
+++ radiant/common/models.py
@@ -9,11 +9,12 @@ class AbstractRadiantModel(models.Model):
     content = models.TextField(blank=True)
 
     # facebook metatags
-    og_title = models.CharField('og:title', max_length=255, blank=True, null=True)
+    og_title = models.CharField('og:title', max_length=255, blank=True, null=True,
+                                help_text='Facebook share title')
     og_type = models.CharField('og:title', max_length=255, blank=True, null=True,
                                default='article')
     og_description = models.CharField('og:description', max_length=255, blank=True,
-                                      null=True)
+                                      null=True, help_text='Facebook share description')
     og_image = models.ImageField(upload_to='ogtags/', blank=True,
                                  help_text='An image has to be at least 50px by 50px, '
                                            'but preferably bigger than 200px by 200px. '
