86673ea509ae7e9bd050acdd9514e1a60b5a6ce9
Victor Rocha <victor@rochapps.com>
2016-01-15 23:23:37 -0500

    [latest episodes] Grab one unreleased and two released episodes.

--- radiant/profiles/templatetags/profiles_tags.py
+++ radiant/profiles/templatetags/profiles_tags.py
@@ -1,11 +1,15 @@
 from django import template
 
-register = template.Library()
-
 from radiant.profiles.models import RadiantHuman
+
+
+register = template.Library()
 
 
 @register.assignment_tag
 def get_latest_episodes():
-    humans = RadiantHuman.objects.all()
-    return humans
+    live_episodes = RadiantHuman.objects.filter(status=RadiantHuman.LIVE).order_by('-release_date')[:2]
+    unreleased_episode = RadiantHuman.objects.filter(status=RadiantHuman.UNRELEASED).order_by('release_date').first()
+    episodes = [episode for episode in live_episodes]
+    episodes.append(unreleased_episode)
+    return episodes
