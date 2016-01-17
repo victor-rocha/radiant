3c146d789eebb8444681e91abb6a5d14e48de561
Victor Rocha <victor@rochapps.com>
2016-01-17 13:39:09 -0500

    [angular slider] Set current video to center

--- radiant/profiles/views.py
+++ radiant/profiles/views.py
@@ -16,4 +16,9 @@ class RadiantHumanListView(ListView):
         qs = super(RadiantHumanListView, self).get_queryset()
         return qs.exclude(status=RadiantHuman.DRAFT).order_by('release_date')
 
-    # def
+    def get_context_data(self, **kwargs):
+        ctx = super(RadiantHumanListView, self).get_context_data(**kwargs)
+        episodes = ctx['object_list']
+        current_episodes_index = episodes.filter(status=RadiantHuman.LIVE).count()
+        ctx['current_episodes_index'] = current_episodes_index
+        return ctx
