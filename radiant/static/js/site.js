893da7e788cc6299587208a5f2d265a8e238fd20
Victor Rocha <victor@rochapps.com>
2016-01-30 13:35:29 -0500

    [homepage] Fixes read more button
    Not all the text was being shown after clicking read more.
    Passing the true paraeter to outerHeight() fixed the problem.

--- radiant/static/js/site.js
+++ radiant/static/js/site.js
@@ -91,7 +91,7 @@
         $ps = $up.find("p:not('.read-more')");
         // measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
         $ps.each(function() {
-            totalHeight += $(this).outerHeight();
+            totalHeight += $(this).outerHeight(true);
         });
 
         $up
